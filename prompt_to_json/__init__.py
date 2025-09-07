0e9621145c9a81aacfb835c167f7c1edfab055ee
AADARSH GAIKWAD <aadarshgaikwad96@gmail.com>
2025-09-07 16:14:09 -0500

    build: black fail fix

--- prompt_to_json/__init__.py
+++ prompt_to_json/__init__.py
@@ -1,10 +1,7 @@
+
 from .converter import PromptToJSON
 from .schema import PromptJSON
 
 __version__ = "0.1.1"
 
 __all__ = ["PromptToJSON", "PromptJSON"]
-
-# __version__ = "0.1.0"
-
-# __all__ = ["PromptToJSON"]
